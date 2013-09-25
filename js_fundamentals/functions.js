89feeea1a4d47dd2ad1e6a36a4a1be514cba1fe5
hippli <philip.e.harrison@gmail.com>
2013-09-24 23:04:04 -0400

    commit functions

--- js_fundamentals/functions.js
+++ js_fundamentals/functions.js
@@ -9,6 +9,16 @@
 // outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 // Call that function 3 times with 3 different values for the arguments.
 
+function tellFortune(children, partner, city, jobTitle) {
+
+console.log("You will be a " + jobTitle + " in " + city + " and married to "+ partner + "with "+ children + " kids!");
+
+}
+
+tellFortune(1, "susan", "toronto", "product manager");
+tellFortune(1, "samantha", "toronto", "entrepreneur");
+tellFortune(1, "jamie", "toronto", "baker");
+
 // EXERCISE: The Age Calculator
 
 // Forgot how old you are? Calculate it!
@@ -20,6 +30,16 @@
 // Call the function three times with different sets of values.
 // Bonus: Figure out how to get the current year in JavaScript instead of passing it in.
 
+
+function calculateAge(birth_year, current_year) {
+	var age = current_year - birth_year;
+	console.log("You are either " + age + " or " + (age - 1));
+}
+
+console.log(calculateAge(1971, 2013));
+console.log(calculateAge(1980, 2013));
+console.log(calculateAge(1927, 2013));
+
 // EXERCISE: The Lifetime Supply Calculator
 
 // Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
@@ -30,6 +50,17 @@
 // outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
 // Call that function three times, passing in different values each time.
 // Bonus: Accept floating point values for amount per day, and round the result to a round number.
+
+function calculateSupply(age, perDay) {
+	var maxAge = 100;
+	var amount = (maxAge - age) * 365 * perDay;
+	console.log("You will need " + amount + " to last you until the ripe old age of " + maxAge);
+}
+
+calculateSupply(42, 3);
+calculateSupply(42, 5);
+calculateSupply(25, 6);
+
 
 // EXERCISE: The Geometrizer
 
@@ -57,3 +88,17 @@
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
 
+function celsiusToFarenheight(celcius) {
+	cToF = celcius * 9 / 5 + 32;
+	console.log(celcius + "°C is " + cToF + "°F");
+}
+
+celsiusToFarenheight(15);
+
+
+function farenheightToCelcius(fahrenheit) {
+	fToC = (fahrenheit - 32) * 5 / 9;
+	console.log(fahrenheit + "°F is " + fToC + "°C");
+}
+
+farenheightToCelcius(15);
